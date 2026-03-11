5b0b5451bd32e55c104f6f1d807db7a15a4455fb
Utsav Mistry <utsavamistry30@gmail.com>
2026-03-11 22:34:18 +0530

    Add project files.

---
+++ ShardLegacy.Server/Program.cs
@@ -0,0 +1,34 @@
+namespace ShardLegacy.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            app.UseDefaultFiles();
+            app.MapStaticAssets();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+            app.MapControllers();
+            app.MapFallbackToFile("/index.html");
+
+            app.Run();
+        }
+    }
+}
